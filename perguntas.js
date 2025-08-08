eee0d134b2ebe79ea9d000275670479ab495b952
juwck <julia.werneck.santos@escola.pr.gov.br>
2025-08-08 11:56:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Astronomia', 
+    'De onde veio o carbono, composto tão importante da nossa composição?', 
+    'Veio do processo de fusão do interior das estrelas' 
+)
+
+criaCartao(
+    'Língua Portuguesa', 
+    'Qual o antônimo de estreito?', 
+    'Amplo' 
+)
+
+criaCartao(
+    'Filosofia', 
+    'Por que Sócrates foi condenado?', 
+    'Corromper a juventude e não reconhecer os deuses'
+)
+
+criaCartao(
+    'Física', 
+    'O que é SI?', 
+    'Sistema Internacional de Unidade' 
+)
+
